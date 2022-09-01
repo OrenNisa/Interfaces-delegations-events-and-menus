e6a232410e2a6d74651a5eddb7c916fe46152afb
David Novizky <dvdn.99@gmail.com>
2022-09-01 20:32:12 +0300

    Interfaces part finished

---
+++ Ex04.Menus.Test/ExampleFunctions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Ex04.Menus.Test
+{
+    internal class ExampleFunctions
+    {
+        internal class ShowDateTime
+        {
+            public void ShowTime()
+            {
+                Console.WriteLine(DateTime.Now.ToString("HH:mm:ss"));
+            }
+
+            public void ShowDate()
+            {
+                Console.WriteLine(DateTime.Now.ToString("dd-MM-yyyy"));
+            }
+        }
+
+        internal class VersionAndSpaces
+        {
+            public void CountSpaces()
+            {
+                Console.WriteLine("Please enter your sentence:");
+                string userSentence = Console.ReadLine();
+                int numOfSpaces = 0;
+
+                foreach (char charInSentence in userSentence)
+                {
+                    if (char.IsWhiteSpace(charInSentence))
+                    {
+                        numOfSpaces++;
+                    }
+                }
+
+                Console.WriteLine("There are {0} Spaces in your sentence.", numOfSpaces);
+            }
+
+            public void ShowVersion()
+            {
+                Console.WriteLine("Version: 22.3.4.8650");
+            }
+        }
+    }
+}
--- Ex04.Menus.Test/Program.cs
+++ Ex04.Menus.Test/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace Ex04.Menus.Test
+﻿using Ex04.Menus;
+using Ex04.Menus.Interfaces;
+
+namespace Ex04.Menus.Test
 {
     public class Program
     {
         public static void Main()
         {
+            MainMenu m_MainMenu = new MainMenu("Interfaces Main Menu");
+            MenuItem mi1 = new MenuItem("Version and Spaces", m_MainMenu);
+            MenuItem mi2 = new MenuItem("Show Date/Time", m_MainMenu);
+
+            Function fm11 = new Function("Count Spaces", new ExampleFunctions.VersionAndSpaces().CountSpaces, m_MainMenu);
+            Function fm12 = new Function("Show Version", new ExampleFunctions.VersionAndSpaces().ShowVersion, m_MainMenu);
+            mi1.AddItem(fm11);
+            mi1.AddItem(fm12);
+
+            Function fm21 = new Function("Show Time", new ExampleFunctions.ShowDateTime().ShowTime, m_MainMenu);
+            Function fm22 = new Function("Show Date", new ExampleFunctions.ShowDateTime().ShowDate, m_MainMenu);
+            mi2.AddItem(fm21);
+            mi2.AddItem(fm22);
+
+            m_MainMenu.AddMenuItem(mi1);
+            m_MainMenu.AddMenuItem(mi2);
+            m_MainMenu.Show();
         }
     }
 }
